33c4d9a766209e0aab292c98c0108849f0769341
Jangganghyeon <inxangxy@gmail.com>
2025-06-05 15:50:29 +0900

    Create 00_sehwa.py

---
+++ pages/00_sehwa.py
@@ -0,0 +1,2 @@
+import straemlit as st
+st.image("https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExYnB1Y254YXU5em11am84Mnc0dnBwa3ZjNXB1NXh1amhlbHV6aXJzaiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/gZ0GEz2HefV4s/giphy.gif")
